e6a4873bc96864b36bed30782c1a77c282bb7c81
Drew Mika <drew.mika@onbase.com>
2016-05-04 16:42:13 -0400

    added routing

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-var express = require("express"),
-              router = express.Router();
+var express = require("express");
 var app     = express();
 var morgan  = require("morgan");
 var port    = process.env.PORT || 3333;
 
+var mainRouter = require('./routes/mainRouter.js');
+
 app.use(express.static('public'));
+app.set('views','views');
 app.use(morgan('combined'));
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+app.use('/', mainRouter);
 
 app.listen(port);
 console.log('listening on port ' + port + '...');
